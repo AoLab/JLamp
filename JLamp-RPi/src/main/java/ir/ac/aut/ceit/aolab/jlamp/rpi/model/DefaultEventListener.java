eda5ff7b69e2810e086761f7759e9bc72bce5208
Iman Tabrizian <tabrizian@outlook.com>
2016-05-31 17:11:59 +0430

    Improves logging message of Default Event.

--- JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/model/DefaultEventListener.java
+++ JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/model/DefaultEventListener.java
@@ -3,18 +3,25 @@ package ir.ac.aut.ceit.aolab.jlamp.rpi.model;
 import ir.ac.aut.ceit.aolab.LampEventFamily;
 import ir.ac.aut.ceit.aolab.OnIEvent;
 import ir.ac.aut.ceit.aolab.TurnEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by root on 5/17/16.
  */
 public class DefaultEventListener implements LampEventFamily.Listener {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultEventListener.class);
+
 	@Override
 	public void onEvent(TurnEvent turnEvent, String s) {
+		LOG.info("Received turn event");
 		Lamp.getLampById(turnEvent.getId()).sendLampCommand(turnEvent.getStatus() ? 1 : 0);
 	}
 
 	@Override
 	public void onEvent(OnIEvent onIEvent, String s) {
+		LOG.info("Received onI event");
 		Lamp.getLampById(onIEvent.getId()).sendLampCommand(1);
 		try {
 			Thread.sleep(onIEvent.getCommand() * 1000);
