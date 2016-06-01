d55728a505ab925f0a741deb56641a2c3693bdcc
Iman Tabrizian <tabrizian@outlook.com>
2016-06-01 15:58:52 +0430

    Minor improvements

--- JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/model/DefaultLampEventListener.java
+++ JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/model/DefaultLampEventListener.java
@@ -16,32 +16,32 @@ import static ir.ac.aut.ceit.aolab.jlamp.rpi.model.Lamp.getLampById;
  */
 public class DefaultLampEventListener implements LampEventFamily.Listener {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DefaultLampEventListener.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultLampEventListener.class);
 
-	@Override
-	public void onEvent(TurnEvent turnEvent, String s) {
-		LOG.info("Received turn event");
-		getLampById(turnEvent.getId()).sendLampCommand(turnEvent.getStatus() ? 1 : 0);
-	}
+    @Override
+    public void onEvent(TurnEvent turnEvent, String s) {
+        LOG.info("Received turn event");
+        Lamp.getLampById(turnEvent.getId()).sendLampCommand(turnEvent.getStatus() ? 1 : 0);
+    }
 
-	@Override
-	public void onEvent(OnIEvent onIEvent, String s) {
-		LOG.info("Received onI event");
-		getLampById(onIEvent.getId()).sendLampCommand(1);
-		try {
-			Thread.sleep(onIEvent.getInterval() * 1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		getLampById(onIEvent.getId()).sendLampCommand(0);
-	}
+    @Override
+    public void onEvent(OnIEvent onIEvent, String s) {
+        LOG.info("Received onI event");
+        getLampById(onIEvent.getId()).sendLampCommand(1);
+        try {
+            Thread.sleep(onIEvent.getInterval() * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getLampById(onIEvent.getId()).sendLampCommand(0);
+    }
 
-	@Override
-	public void onEvent(StatusEvent statusEvent, String s) {
-		LOG.info("Received status of lamp event");
-		Boolean status = Lamp.getLampById(statusEvent.getId()).getLampStatus();
+    @Override
+    public void onEvent(StatusEvent statusEvent, String s) {
+        LOG.info("Received status of lamp event");
+        Boolean status = Lamp.getLampById(statusEvent.getId()).getLampStatus();
         StatusEvent statusEventResponse = new StatusEvent(status ? "true" : "false");
         KaaController.getInstance().sendStatusEvent(statusEventResponse);
-	}
+    }
 
 }
