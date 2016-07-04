6cd0deb1f491985bd96684e55d39ded463ccaad4
Parham Alvani <parham.alvani@gmail.com>
2016-07-05 02:24:30 +0430

    All handlers return Iman response :)

---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/http/IResponse.java
@@ -0,0 +1,34 @@
+package ir.ac.aut.ceit.aolab.jlamp.rest.http;
+
+/**
+ * Created by iman on 5/21/16.
+ */
+public class IResponse {
+	private String error;
+	private String status;
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+}
