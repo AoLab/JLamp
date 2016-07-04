fb6ba77be07372983285892985e0507a84fa2e90
Parham Alvani <parham.alvani@gmail.com>
2016-07-05 02:12:10 +0430

    Add JSONTransmformer to response with JSON :)

---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/http/JsonTransformer.java
@@ -0,0 +1,26 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : JLamp 
+ * 
+ * [] Package Name : ir.ac.aut.ceit.aolab.jlamp.rest.helper
+ *
+ * [] Creation Date : 05-07-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package ir.ac.aut.ceit.aolab.jlamp.rest.http;
+
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+public class JsonTransformer implements ResponseTransformer {
+	private Gson gson = new Gson();
+
+	@Override
+	public String render(Object model) {
+		return gson.toJson(model);
+	}
+}
