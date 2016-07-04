3b4605d1a44fd0c5b34492ff68921577ba02e110
Parham Alvani <parham.alvani@gmail.com>
2016-07-05 03:00:47 +0430

    JLamp services were added.

---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/services/LampService.java
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : JLamp 
+ * 
+ * [] Package Name : ir.ac.aut.ceit.aolab.jlamp.rest.services
+ *
+ * [] Creation Date : 05-07-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package ir.ac.aut.ceit.aolab.jlamp.rest.services;
+
+import ir.ac.aut.ceit.aolab.jlamp.rest.http.IResponse;
+import spark.Request;
+import spark.Response;
+
+public interface LampService extends Service {
+	IResponse onTurnRequest(Request request, Response response);
+
+	IResponse onStatusRequest(Request request, Response response);
+}
---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/services/Service.java
@@ -0,0 +1,17 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : JLamp 
+ * 
+ * [] Package Name : ir.ac.aut.ceit.aolab.jlamp.rest.services
+ *
+ * [] Creation Date : 05-07-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package ir.ac.aut.ceit.aolab.jlamp.rest.services;
+
+public interface Service {
+}
