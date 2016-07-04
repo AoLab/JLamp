d0d055ba63a4ccc4c1fa17941f83258ce89db8f9
Parham Alvani <parham.alvani@gmail.com>
2016-07-05 03:15:38 +0430

    Main class was improved.

--- JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
@@ -1,6 +1,8 @@
 package ir.ac.aut.ceit.aolab.jlamp.rest;
 
 import com.sun.net.httpserver.HttpServer;
+import ir.ac.aut.ceit.aolab.jlamp.rest.kaa.KaaLampService;
+import ir.ac.aut.ceit.aolab.jlamp.rest.routes.LampRoute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,10 +15,12 @@ public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
+		LampRoute.registerRoutes(new KaaLampService());
+
 		try {
 			HttpServer server = HttpServer.create(new InetSocketAddress("192.168.128.90", 4500), 30);
 			server.createContext("/lamp/", new CustomHttpHandler(CustomKaaClient.getKaaClient(), null));
-            server.start();
+			server.start();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
