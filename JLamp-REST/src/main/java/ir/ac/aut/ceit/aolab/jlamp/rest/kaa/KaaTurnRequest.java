92a4c1c0b6c7235ab7fbf8d42d2bfe61e1ef6ac0
Parham Alvani <parham.alvani@gmail.com>
2016-07-05 02:46:13 +0430

    KaaTrunRequest PoJo was added.

---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/kaa/KaaTurnRequest.java
@@ -0,0 +1,35 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : JLamp 
+ * 
+ * [] Package Name : ir.ac.aut.ceit.aolab.jlamp.rest.kaa
+ *
+ * [] Creation Date : 05-07-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+package ir.ac.aut.ceit.aolab.jlamp.rest.kaa;
+
+public class KaaTurnRequest {
+	private String id;
+	private boolean status;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
