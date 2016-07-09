e0ea6d557e230289a2a09ff8dab65029083f784e
Parham Alvani <parham.alvani@gmail.com>
2016-07-10 03:43:56 +0430

    Our server run on 4500 :)

--- JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
@@ -2,10 +2,12 @@ package ir.ac.aut.ceit.aolab.jlamp.rest;
 
 import ir.ac.aut.ceit.aolab.jlamp.rest.kaa.KaaLampService;
 import ir.ac.aut.ceit.aolab.jlamp.rest.routes.LampRoute;
+import spark.Spark;
 
 public class Main {
 
     public static void main(String[] args) {
+        Spark.port(4500);
         LampRoute.registerRoutes(new KaaLampService());
     }
 
