57a9768f4173dc443d2d40a71c2e9fa88903cdf9
Iman Tabrizian <Tabrizian@outlook.com>
2016-07-14 22:54:06 +0430

    Moves listeners inside kaa package

---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/kaa/DefaultLampEventListener.java
@@ -0,0 +1,13 @@
+package ir.ac.aut.ceit.aolab.jlamp.rest.kaa;
+
+import ir.ac.aut.ceit.aolab.LampEventFamily.Listener;
+import ir.ac.aut.ceit.aolab.jlamp.StatusEvent;
+
+/**
+ * Created by iman on 7/14/16.
+ */
+public class DefaultLampEventListener implements Listener {
+    @Override
+    public void onEvent(StatusEvent statusEvent, String s) {
+    }
+}
---
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/kaa/DefaultPIREventListener.java
@@ -0,0 +1,14 @@
+package ir.ac.aut.ceit.aolab.jlamp.rest.kaa;
+
+import ir.ac.aut.ceit.aolab.jlamp.PIREventClassFamily;
+import ir.ac.aut.ceit.aolab.jlamp.pir.StatusEvent;
+
+/**
+ * Created by iman on 7/14/16.
+ */
+public class DefaultPIREventListener implements PIREventClassFamily.Listener {
+    @Override
+    public void onEvent(StatusEvent statusEvent, String s) {
+
+    }
+}
