f51983946241ccaa2ff506b7af346e70efefa1ad
Parham Alvani <parham.alvani@gmail.com>
2016-08-01 21:33:50 -0700

    Use port 1373 :)

--- JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
+++ JLamp-REST/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rest/Main.java
@@ -7,7 +7,7 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) {
-        Spark.port(35000);
+        Spark.port(1373);
         LampRoute.registerRoutes(new KaaLampService());
     }
 
