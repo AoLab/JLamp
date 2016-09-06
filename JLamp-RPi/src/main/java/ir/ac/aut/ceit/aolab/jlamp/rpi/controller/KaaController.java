c2fe7fd3f67c1a7fdba342753f43928e8108c088
Iman Tabrizian <Tabrizian@outlook.com>
2016-09-06 20:03:57 +0430

    Removes some old code

--- JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/controller/KaaController.java
+++ JLamp-RPi/src/main/java/ir/ac/aut/ceit/aolab/jlamp/rpi/controller/KaaController.java
@@ -1,8 +1,5 @@
 package ir.ac.aut.ceit.aolab.jlamp.rpi.controller;
 
-import ir.ac.aut.ceit.aolab.LampEventFamily;
-import ir.ac.aut.ceit.aolab.jlamp.PIREventClassFamily;
-import ir.ac.aut.ceit.aolab.jlamp.StatusEvent;
 import ir.ac.aut.ceit.aolab.jlamp.rpi.model.DefaultLampEventListener;
 import ir.ac.aut.ceit.aolab.jlamp.rpi.model.DefaultNotificationListener;
 import ir.ac.aut.ceit.aolab.jlamp.rpi.model.DefaultPIREventListener;
@@ -11,7 +8,6 @@ import org.kaaproject.kaa.client.DesktopKaaPlatformContext;
 import org.kaaproject.kaa.client.Kaa;
 import org.kaaproject.kaa.client.KaaClient;
 import org.kaaproject.kaa.client.SimpleKaaClientStateListener;
-import org.kaaproject.kaa.client.event.EventFamilyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,29 +48,10 @@ public class KaaController {
 
     public void start() {
         kaaClient.start();
-        EventFamilyFactory eventFamilyFactory = kaaClient.getEventFamilyFactory();
-        LampEventFamily lampEventFamily = eventFamilyFactory.getLampEventFamily();
-        lampEventFamily.addListener(new DefaultLampEventListener());
-        PIREventClassFamily pirEventClassFamily = eventFamilyFactory.getPIREventClassFamily();
-        pirEventClassFamily.addListener(new DefaultPIREventListener());
-
-
         kaaClient.attachUser("userExternalId",
                 "userAccessToken", response -> System.out.println("Attach response" + response.getResult()));
 
         kaaClient.addNotificationListener(new DefaultNotificationListener());
-        char c;
-        c = Serial.getSerialInstance().readChar();
-        LOG.info("READLINE " + c);
-        c = Serial.getSerialInstance().readChar();
-        LOG.info("READLINE " + c);
-        c = Serial.getSerialInstance().readChar();
-        LOG.info("READLINE " + c);
-        c = Serial.getSerialInstance().readChar();
-        LOG.info("READLINE " + c);
-        c = Serial.getSerialInstance().readChar();
-        LOG.info("READLINE " + c);
-
     }
 
     public static KaaController getInstance() {
@@ -83,20 +60,5 @@ public class KaaController {
 
         return instance;
     }
-
-    public void sendStatusEvent(StatusEvent statusEvent) {
-        EventFamilyFactory eventFamilyFactory = kaaClient.getEventFamilyFactory();
-        LampEventFamily lampEventFamily = eventFamilyFactory.getLampEventFamily();
-        lampEventFamily.sendEventToAll(statusEvent);
-
-    }
-
-
-    public void sendPIRStatusEvent(ir.ac.aut.ceit.aolab.jlamp.pir.StatusEvent pirStatusEvent) {
-        EventFamilyFactory eventFamilyFactory = kaaClient.getEventFamilyFactory();
-        PIREventClassFamily pirEventFamily = eventFamilyFactory.getPIREventClassFamily();
-        pirEventFamily.sendEventToAll(pirStatusEvent);
-    }
-
 
 }
